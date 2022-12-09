e47966e3471a75ee0f53d5830abf11a836368b2c
Blessman-Newton <blessmannewton0@gmail.com>
2022-12-08 21:50:02 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+/**
+ *main - Entry
+ *Return: Always 0
+ */
+#include <stdio.h>
+int main(void)
+{
+char a;
+int b;
+float c;
+long int d;
+long long int e;
+
+printf("Size of a char: %c byte(s)", sizeof(a));
+printf("Size of a int : %d byte(s)", sizeof(b));
+printf("Size of a float : %f byte(s)", sizeof(c));
+printf("Size of a long int : %ld byte(s)", sizeof(d));
+printf("Size of long long iny : %lld bytr(s)", sizeof(e));
+return (0);
+}
