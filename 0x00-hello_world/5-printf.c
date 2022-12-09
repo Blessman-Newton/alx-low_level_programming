ed9301da88c3849852d2c248cea3b651ea829f86
Blessman-Newton <blessmannewton0@gmail.com>
2022-12-08 21:16:12 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 /**
  *main - Entry
- *Return: Always
+ *Return: Always 0
  */
 #include <stdio.h>
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n ");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
