88cc76a4763849558459ef17502b899f386b2eae
Blessman-Newton <blessmannewton0@gmail.com>
2022-12-10 19:39:22 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+/**
+ *main - Enter
+ *Return: Always
+ */
+#include <stdio.h>
+int main(void)
+{
+char alpha;
+for (alpha ='a' ; alpha <= 'z' ; alpha++)
+putchar(alpha)
+}
+putchar('\n')
+for (alpha = 'A' ; alpha <= 'Z' ; alpha++)
+{
+putchar(alpha)
+}
+putchar('\n');
+return (0);
+}
+    
