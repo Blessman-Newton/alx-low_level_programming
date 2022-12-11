3468ea13258bb03f4c301b274d6a541099382cc1
Blessman-Newton <blessmannewton0@gmail.com>
2022-12-11 13:32:31 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+/**
+ * main - A  program that prints all single digit numbers
+ * Return: 0 SUCCESSFUL MEG
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
