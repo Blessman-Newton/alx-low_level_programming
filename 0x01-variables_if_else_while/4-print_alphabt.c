6bfb94265bc7234190215b40e854801d976c016a
Blessman-Newton <blessmannewton0@gmail.com>
2022-12-10 20:37:52 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+/**
+ * main - Entry
+ * Return: Always
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	char alpha;
+	
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+	{
+		if ((alpha != 'e') && (alpha != 'q'))
+		{
+			putchar(alpha);
+		}
+		putchar('\n');
+		return (0);
+	}
+}
