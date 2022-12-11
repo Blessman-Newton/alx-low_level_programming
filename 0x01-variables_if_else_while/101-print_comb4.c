230f96c1a23f9bf3a3e438f9a6af8bda39ff5518
Blessman-Newton <blessmannewton0@gmail.com>
2022-12-11 15:54:48 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+/**
+ * main - Entry point
+ * Returen: Always 0:
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0 ; a < 8 ; a++)
+	{
+		for (b = a + 1 ; b < 9 ; b++)
+		{
+			for (c = b + 1 ; c < 10 ; c++)
+			{
+				if (a != b && a != c && b != c)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+					if (a + b + c < 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
