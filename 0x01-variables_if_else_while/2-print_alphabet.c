4adb0610e903417cfb5be4b603baec6de26e0b98
Blessman-Newton <blessmannewton0@gmail.com>
2022-12-10 19:22:51 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,10 +5,11 @@
 #include <stdio.h>
 int main(void)
 {
-char alpha = 'a';
- for (alpha = 'a'; alpha <= 'z'; alpha++)
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
 {
 putchar(alpha);
 }
+putchar('\n');
 return (0);
 }
