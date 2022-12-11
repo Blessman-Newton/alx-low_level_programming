febda8c33342fa53de33cb336f6c4f3228e44e5f
Blessman-Newton <blessmannewton0@gmail.com>
2022-12-11 14:01:40 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+/**
+ * main -A program that print the lowercase alphabet in revers
+ * Return: Always return 0 SUCCESS MGE
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char z;
+
+	for (z = 'z' ; z >= 'a' ; a--)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+	return (0);
+}
