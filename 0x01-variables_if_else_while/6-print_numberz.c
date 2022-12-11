c250058414b36c45b6019dd343c372598361ca6a
Blessman-Newton <blessmannewton0@gmail.com>
2022-12-11 13:50:01 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+/**
+ * main - A program that prints all single digit number
+ * Return: Always 0 SUCCESS MGE
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
