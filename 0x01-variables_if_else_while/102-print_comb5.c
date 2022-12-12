67d73583014cce51963144dec5d893f2ae43d0fb
Blessman-Newton <blessmannewton0@gmail.com>
2022-12-11 17:06:36 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+/**
+ * main - all possible combinations of two two-digit number
+ * Return: Always 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0 ; n1 <= 98 ; n1++)
+	{
+		for (n2 = n1 + 1 ; n2 <= 99 ; n2++)
+		{
+			putchar((n1 % 10) + '0');
+			putchar((n1 % 10) + '0');
+			putchar(' ');
+			putchar((n2 % 10) + '0');
+			putchar((n2 % 10) + '0');
+
+			if (n1 == 98 && n2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
