9e026ee2bd5fb0c85838d15e66247b6c3fc2b353
Blessman-Newton <blessmannewton0@gmail.com>
2022-12-11 17:38:57 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+/**
+ * main - Write a C program that prints exactly and that piece
+ * of art is useful" - Dora Korpar, 2015-10-19
+ * , followed by a new line, to the standard error.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}
