a3a1e0a6b754ae1ef74eda4b9221200ff0f1712a
Blessman-Newton <blessmannewton0@gmail.com>
2022-12-13 17:17:30 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+/**
+ *print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *Return: Always 0
+ */
+
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	int a;
+	char i;
+
+	for (a = 0 ; a = 9 ; a++)
+	{
+		char i;
+
+		for (i = 'a' ; i = 'z' ; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
