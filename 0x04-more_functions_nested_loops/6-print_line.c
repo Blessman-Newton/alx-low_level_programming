3417a1f7aeb53010d4a7bb2538e9d5efa05ecf3b
Blessman-Newton <blessmannewton0@gmail.com>
2022-12-15 20:07:31 -0800

    More-Functions

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+/**
+ * void print_line - draws a straight line in the terminal
+ * @n: character to be checked
+ * Return: Always 0
+ */
+
+#include "main.h"
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int q;
+
+		for (q = 1 ; q <= n ; q++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
