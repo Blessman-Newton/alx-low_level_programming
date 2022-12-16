d4488bd5a3c0f3a244a285dc153c078fbc7b1fd6
Blessman-Newton <blessmannewton0@gmail.com>
2022-12-15 19:04:49 -0800

    More-Functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * Return: Always 0
+ */
+
+#include "main.h"
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return (0);
+}
