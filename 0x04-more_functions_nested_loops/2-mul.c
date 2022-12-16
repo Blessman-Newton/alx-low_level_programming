c4e789cfc906d885dc199ec55bec9118c31383c1
Blessman-Newton <blessmannewton0@gmail.com>
2022-12-15 18:58:05 -0800

    More-Functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+/**
+ * mul - multiplies two integers
+ * @a: character to be checked
+ * @b: character to be checked
+ * Return: Always 0
+ */
+
+#include "main.h"
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
