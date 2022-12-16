0434a77b72ba62a7903d867f0618eb6a53ef78b4
Blessman-Newton <blessmannewton0@gmail.com>
2022-12-15 19:42:40 -0800

    More-Functions

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * Return: Always 0
+ */
+
+#include "main.h"
+
+void more_numbers(void)
+{
+	int a;
+	int i;
+
+	for (a = 0 ; a <= 10 ; a++)
+	{
+		for (i = 0 ; i <= 14 ; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
