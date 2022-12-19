485ec1ee3fcbaa092a69cd4c50615bf09375e6df
Blessman-Newton <blessmannewton0@gmail.com>
2022-12-19 11:50:11 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+/**
+ * swap_int - function that swaps the values of two integers
+ * @a: character to be check
+ * @b: character to be check
+ */
+
+#include "main.h"
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
