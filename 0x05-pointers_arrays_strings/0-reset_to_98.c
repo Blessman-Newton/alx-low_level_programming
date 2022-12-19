3c252839e2a59caea92ec0ae94ec3512a4f25a95
Blessman-Newton <blessmannewton0@gmail.com>
2022-12-19 11:37:14 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+/**
+ * reset_to_98 - function that takes a pointer to an
+ * int as parameter and updates the value it
+ * points to to 98
+ * @n: character to ne checked
+ */
+
+#include "main.h"
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
