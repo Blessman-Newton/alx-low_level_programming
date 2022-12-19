c2173b1a80a5f891a37111a855c0d07b8a0e56e5
Blessman-Newton <blessmannewton0@gmail.com>
2022-12-19 12:30:24 -0800

    Pointer

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+/**
+ * _puts - function that prints a string, followed by a new
+ * @str: string to evaluate
+ */
+
+#include "main.h"
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] <= '\0' ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_puchar('\n');
+}
