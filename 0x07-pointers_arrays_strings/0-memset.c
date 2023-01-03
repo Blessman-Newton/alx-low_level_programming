157271c1bb75f5f56b3990746075a9272f02c797
Blessman-Newton <blessmannewton0@gmail.com>
2023-01-03 15:45:16 -0800

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+/**
+ * *_memset - function that fills memory with a constant byt
+ * @s: memory area to be fill
+ * @b: area to be copy
+ * @n: number of b to copy
+ *
+ * Return: return s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n ; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
