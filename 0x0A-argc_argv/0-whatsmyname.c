5bcfe43fca9038c561e8437eab3aa27b2298d350
Blessman-Newton <blessmannewton0@gmail.com>
2023-01-09 17:06:55 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 /**
  * main - prints its name, followed by a new line
- * @argc: number of argurment 
+ * @argc: number of argurment
  * @argv: array of argurment
  * Return: always return a successful mge
  */
 #include "main.h"
+#include <stdio.h>
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	_putchar("%s\n", *argv);
+	printf("%s\n", *argv);
 	return (0);
 }
