f7f9fb994c60e5967d7a35e9e50a6d874a91429f
Blessman-Newton <blessmannewton0@gmail.com>
2023-01-09 16:43:34 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+/**
+ * main - prints its name, followed by a new line
+ * @argc: number of argurment 
+ * @argv: array of argurment
+ * Return: always return a successful mge
+ */
+#include "main.h"
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	_putchar("%s\n", *argv);
+	return (0);
+}
