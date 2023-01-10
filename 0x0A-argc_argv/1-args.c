b0139b7d83c6aba7d705f67ec5baa3ace260b188
Blessman-Newton <blessmannewton0@gmail.com>
2023-01-09 16:53:20 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: number of argument
+ * @argv: array of argument
+ * Return: always return 0 (success)
+ */
+#include "main.h"
+
+int main(int argc, char *argv __attribute__((unussed)))
+{
+	_putchar("%d\n", argc - 1);
+	return (0);
+}
